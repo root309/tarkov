646bad091be6184c5c0c61454aef845d84abc8c2
root309 <choco00101x@gmail.com>
2023-09-26 04:05:07 +0900

    Add Output the price of item

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate serenity;
+extern crate reqwest;
+extern crate serde_json;
 
 use serenity::{
     async_trait,
@@ -6,7 +8,7 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-
+use serde_json::Value;
 use dotenv::dotenv;
 use std::env;
 
@@ -15,8 +17,9 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!a" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "A!").await {
+        if msg.content == "!btc" {
+            let price = get_btc_price().await;
+            if let Err(why) = msg.channel_id.say(&ctx.http, format!("Current BTC Price: {}", price)).await {
                 println!("Error sending message: {:?}", why);
             }
         }
@@ -42,5 +45,33 @@ async fn main() {
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
+    }
+}
+
+async fn get_btc_price() -> String {
+    let query = r#"
+        {
+            items(name: "MoonShine") {
+                id
+                name
+                avg24hPrice
+            }
+        }
+    "#;
+
+    let client = reqwest::Client::new();
+    match client.post("https://api.tarkov.dev/graphql")
+        .header("Content-Type", "application/json")
+        .body(format!(r#"{{"query": "{}"}}"#, query))
+        .send()
+        .await {
+            Ok(response) => {
+                if let Ok(json) = response.json::<Value>().await {
+                    json["data"]["items"][0]["avg24hPrice"].to_string()
+                } else {
+                    "Failed to parse response".to_string()
+                }
+            },
+            Err(_) => "Failed to send request".to_string(),
     }
 }
