23dcccd03e390cce6bb95be0517940039c0a4cc4
root309 <choco00101x@gmail.com>
2023-09-26 05:56:13 +0900

    Add explanatory comments to source code

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,14 @@ use serenity::{
 use serde_json::Value;
 use dotenv::dotenv;
 use std::env;
-
+// イベントハンドラの構造体
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    // 新しいメッセージを受信したときのイベントハンドラ
     async fn message(&self, ctx: Context, msg: Message) {
+        // コマンドが受信された場合にビットコインの価格を取得して出力
         if msg.content == "!btc" {
             let price = get_btc_price().await;
             if let Err(why) = msg.channel_id.say(&ctx.http, format!("Current BTC Price: {}", price)).await {
@@ -24,7 +26,7 @@ impl EventHandler for Handler {
             }
         }
     }
-
+    // ボット接続イベント
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -32,21 +34,26 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    // .envファイルから環境変数を読み込む
     dotenv().ok();
 
+    // DISCORD_TOKEN環境変数からトークンを取得する
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
 
+    // クライアントを作成
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .framework(StandardFramework::new())
         .await
         .expect("Err creating client");
 
+    // クライアントを開始し、エラーが発生した場合はエラーを出力する
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
 }
+// ビットコインの価格を取得する非同期関数
 async fn get_btc_price() -> String {
     let query = r#"
         {
@@ -68,6 +75,7 @@ async fn get_btc_price() -> String {
         "query": query
     });
 
+    // GraphQLエンドポイントにPOSTリクエストを送信し、レスポンスを解析する
     match client.post("https://api.tarkov.dev/graphql")
         .header("Content-Type", "application/json")
         .json(&body)  // bodyをJSONとして送信する
@@ -76,6 +84,7 @@ async fn get_btc_price() -> String {
             Ok(response) => {
                 if let Ok(json) = response.json::<Value>().await {
                     println!("{:?}", json);
+                    // アイテムとトレーダーの価格情報を解析し、整形する
                     let item = &json["data"]["items"][0];
                     let trader_prices = &item["traderPrices"];
                     let prices: Vec<String> = trader_prices.as_array().unwrap_or(&vec![]).iter().map(|tp| {
