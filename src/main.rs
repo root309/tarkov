e7ec08a9d37b86529ec60b07f62c6530514fdf92
root309 <choco00101x@gmail.com>
2023-09-26 03:17:47 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+        if msg.content == "!a" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "A!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
